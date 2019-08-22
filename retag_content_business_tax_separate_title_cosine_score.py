82669c37e201a277e6095dd868788d34c213d048
Oscar Wyatt <oscar.wyatt@digital.cabinet-office.gov.uk>
2019-08-22 10:38:58 +0100

    Single script to fetch all mistagged content and attempt to move it

--- retag_content_business_tax_separate_title_cosine_score.py
+++ retag_content_business_tax_separate_title_cosine_score.py
@@ -12,14 +12,9 @@ class Node:
         self.children = []
         self.all_sibs_and_children = None
     def unique_title(self):
-        # Some taxa have identical names so using the title as the
-        # key of a dictionary will cause problems
         return self.content_id[:3] + " " + self.title
     def title_and_parent_title(self):
-        if self.parent:
-            return self.parent.title + "/" + self.title
-        else:
-            return self.title
+        return self.parent.title + "/" + self.title
     def recursive_children(self):
         if self.children:
             results = []
@@ -31,14 +26,8 @@ class Node:
     def all_siblings_and_children(self):
         if self.all_sibs_and_children is None:
             results = []
-            # This is a slightly hacky way of not returning the entire tree if the node
-            # is a level 1 taxon. E.g. if the taxon has no parent it's level 1 so only return it's children
-            # rather than all it's siblings and their children (which would be the entire tree)
-            if not self.parent:
-                results.append(self.recursive_children())
-            else:
-                for node in self.parent.children:
-                    results.append(node.recursive_children())
+            for node in self.parent.children:
+                results.append(node.recursive_children())
             flattened_results = [item for sublist in results for item in sublist]
             # Remove self from results
             self.all_sibs_and_children = [result for result in flattened_results if result.content_id != self.content_id]
@@ -56,49 +45,14 @@ class Tree:
             for taxon in taxons:
                 node = Node(taxon, self.nodes)
                 self.nodes[node.content_id] = node
-    def find(self, taxon_content_id):
-        return self.nodes[taxon_content_id]
-
-# This identifies content items in a taxon that might be out of place
-# Content items are flagged if their mean score is above a particular threshold (default 0.65).
-def get_misplaced_content_in_taxon(content, taxon, similarity_threshold = 0.65):
-    print('Looking for misplaced content in taxon: ', taxon.title)
-    taxon_embeddings = get_embedded_sentences_for_taxon(content, taxon)
-    distances_between_all_content_item_pairs = pairwise_distances(
-        taxon_embeddings,
-        metric = 'cosine',
-        n_jobs = -1
-    )
-    content_for_taxon = get_content_for_taxon(content, taxon).copy()
-    content_for_taxon['mean_cosine_score'] = distances_between_all_content_item_pairs.mean(axis=1)
-    misplaced_items = content_for_taxon.loc[content_for_taxon['mean_cosine_score'] > similarity_threshold].copy()
-    misplaced_items["taxon_id"] = taxon.content_id
-    misplaced_items["taxon_title"] = taxon.unique_title()
-    return misplaced_items;
-
-# Finds all content that might be incorrectly tagged
-# Currently hard coded to look in money branch but could look anywhere
-def find_misplaced_items(content, tree):
-    # Content id for money node
-    money_branch_apex_node = tree.find("6acc9db4-780e-4a46-92b4-1812e3c2c48a")
-    taxons_to_search = [money_branch_apex_node] + money_branch_apex_node.all_siblings_and_children()
-    misplaced_items = pd.DataFrame()
-    for taxon in taxons_to_search:
-        misplaced_items_for_taxon = get_misplaced_content_in_taxon(content, tree.find(taxon.content_id))
-        misplaced_items = misplaced_items.append(misplaced_items_for_taxon)
-    problem_content_path = os.path.join(DATADIR, 'problem_content.csv')
-    print("Found " + str(len(misplaced_items)) + " misplaced items. Saving csv to " + problem_content_path)
-    misplaced_items.to_csv(problem_content_path)
-    return misplaced_items;
+    def find(self, content_id):
+        return self.nodes[content_id]
 
 def get_embedded_sentences_for_taxon(content, taxon):
-    return get_content_for_taxon(content, taxon)['combined_text_embedding'].to_list()
-
-def get_content_for_taxon(content, taxon):
     content_taxon_mapping_path = os.path.join(DATADIR, 'content_to_taxon_map.csv')
     content_taxon_mapping = pd.read_csv(content_taxon_mapping_path, low_memory=False)
     content_ids_for_taxon = list(content_taxon_mapping[content_taxon_mapping['taxon_id'] == taxon.content_id]['content_id'])
-    return content[content['content_id'].isin(content_ids_for_taxon)];
+    return content[content['content_id'].isin(content_ids_for_taxon)]['embedded_sentences'].to_list()
 
 def get_score_for_item(content, all_content, taxon):
     embedded_sentences_for_taxon = get_embedded_sentences_for_taxon(all_content, taxon)
@@ -125,7 +79,7 @@ def get_cosine_scores_for_sibling_and_children_taxons(current_taxon, embedded_co
     mean_cosine_score_for_each_taxon = sorted(mean_cosine_scores_for_each_taxon.items(), key=operator.itemgetter(1))
     return (mean_cosine_score_for_each_taxon, all_cosine_scores_for_each_taxon);
 
-# This was an attempt at a better scoring system to get around the fact that mean isn't so great
+
 def get_distance_cosine_scores(mean_cosine_score_for_each_taxon, all_cosine_scores_for_each_taxon):
     distance_cosine_score_for_each_taxon = {}
     for i, scores in enumerate(mean_cosine_score_for_each_taxon):
@@ -177,30 +131,26 @@ def can_be_untagged(current_taxon, content, content_to_retag_base_path):
     for child in current_taxon.recursive_children():
         if child.content_id in taxon_ids_for_content:
             return (True, False, "Tagged to taxon below the current one so we can untag without human intervention");
-    return (True, True, {})
-    # The below was an attempt to see if the other taggings are better than the current one.
-    # This may not be necessary as if this tag has been flagged as possibly incorrect then we
-    # ought to be deleting it by default. This is commented out for interest and because it might
-    # be useful
-    # current_scores = {}
-    # embedded_content = content[content['base_path'] == content_to_retag_base_path].iloc[0,:]['combined_text_embedding']
-    # for taxon_id in taxon_ids_for_content:
-    #     taxon = tree.find(taxon_id)
-    #     # embedded_sentences_for_taxon = get_embedded_sentences_for_taxon(content, taxon)
-    #     # Get the score of the content item against the taxon it's currently in
-    #     scores, mean = get_score_for_item(embedded_content, content, taxon)
-    #     current_scores[taxon.unique_title()] = mean
-    # print(current_scores)
-    # _all_scores_for_current_taxon, score_for_current_taxon = get_score_for_item(embedded_content, content, current_taxon)
-    # scores_for_all_taxons = list(current_scores.values()).sort()
-    # # Not strictly speaking a median but...
-    # if scores_for_all_taxons is None:
-    #     return (False, False, "No cosine similarity scores")
-    # median = scores_for_all_taxons[len(scores_for_all_taxons) / 2]
-    # if score_for_current_taxon >= median:
-    #     return (True, True, current_scores)
-    # else:
-    #     return (False, True, current_scores)
+    # See if there are other, better taggings
+    current_scores = {}
+    embedded_content = content[content['base_path'] == content_to_retag_base_path].iloc[0,:]['embedded_sentences']
+    for taxon_id in taxon_ids_for_content:
+        taxon = tree.find(taxon_id)
+        # embedded_sentences_for_taxon = get_embedded_sentences_for_taxon(content, taxon)
+        # Get the score of the content item against the taxon it's currently in
+        scores, mean = get_score_for_item(embedded_content, content, taxon)
+        current_scores[taxon.unique_title()] = mean
+    print(current_scores)
+    _all_scores_for_current_taxon, score_for_current_taxon = get_score_for_item(embedded_content, content, current_taxon)
+    scores_for_all_taxons = list(current_scores.values()).sort()
+    # Not strictly speaking a median but...
+    if scores_for_all_taxons is None:
+        return (False, False, "No cosine similarity scores")
+    median = scores_for_all_taxons[len(scores_for_all_taxons) / 2]
+    if score_for_current_taxon >= median:
+        return (True, True, current_scores)
+    else:
+        return (False, True, current_scores)
 
 def debugging_entry(base_path, current_taxon, debugging_info):
     return {
@@ -216,8 +166,10 @@ import gzip
 import ijson
 import os
 import pandas as pd
-from sklearn.metrics import pairwise_distances, pairwise_distances_chunked
+from sklearn.metrics import pairwise_distances_chunked
 import operator
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 import math
@@ -237,8 +189,9 @@ labelled = pd.read_csv(labelled_file_path, compression='gzip', low_memory=False)
 clean_content_path = os.path.join(DATADIR, 'embedded_clean_content.pkl')
 content = pd.read_pickle(clean_content_path)
 
-# Load misplaced items
-problem_content = find_misplaced_items(content, tree)
+problem_content_path = os.path.join(DATADIR, 'problem_content.csv')
+problem_content = pd.read_csv(problem_content_path)
+
 
 content_to_retag = []
 content_for_human_verification_to_untag = []
@@ -258,9 +211,9 @@ for index, row in problem_content.iterrows():
             content_for_human_verification_to_untag.append([content_to_retag_base_path, current_taxon.title_and_parent_title(), more_info])
         else:
             content_to_untag.append([content_to_retag_base_path, current_taxon.title_and_parent_title(), more_info])
-            next()
+        next()
     print("Attempting_to_retag: " + content_to_retag_base_path)
-    embedded_content = content[content['base_path'] == content_to_retag_base_path].iloc[0,:]['combined_text_embedding']
+    embedded_content = content[content['base_path'] == content_to_retag_base_path].iloc[0,:]['embedded_sentences']
     # Get the score of the content item against all items
     mean_cosine_score_for_each_taxon, all_cosine_scores_for_each_taxon = get_cosine_scores_for_sibling_and_children_taxons(current_taxon, embedded_content, content)
     best_distance_suggestion, best_distance_cosine_score, distance_cosine_scores = get_distance_cosine_scores(mean_cosine_score_for_each_taxon, all_cosine_scores_for_each_taxon)
---
+++ retag_content_business_tax_tf_idf.py
@@ -0,0 +1,136 @@
+
+class Node:
+    def __init__(self, entry, all_nodes):
+        self.base_path = entry['base_path']
+        self.content_id = entry['content_id']
+        self.title = entry['title']
+        if 'parent_content_id' in entry:
+            self.parent = all_nodes[entry['parent_content_id']]
+            self.parent.children.append(self)
+        else:
+            self.parent = None
+        self.children = []
+    def recursive_children(self):
+        if self.children:
+            results = []
+            for child in self.children:
+                results.append(child.recursive_children())
+            return [item for sublist in results for item in sublist]
+        else:
+            return [self]
+    def all_siblings_and_children(self):
+        results = []
+        for node in self.parent.children:
+            results.append(node.recursive_children())
+        flattened_results = [item for sublist in results for item in sublist]
+        # Remove self from results
+        return [result for result in flattened_results if result.content_id != self.content_id]
+
+
+class Tree:
+    def __init__(self, datadir):
+        self.nodes = {}
+        taxons_path = os.path.join(datadir, 'taxons.json.gz')
+        with gzip.open(taxons_path, mode='rt') as input_file:
+            taxons = ijson.items(input_file, prefix='item')
+            for taxon in taxons:
+                node = Node(taxon, self.nodes)
+                self.nodes[node.content_id] = node
+    def find(self, content_id):
+        return self.nodes[content_id]
+
+def get_combined_text_for_taxon(content, taxon):
+    content_taxon_mapping_path = os.path.join(DATADIR, 'content_to_taxon_map.csv')
+    content_taxon_mapping = pd.read_csv(content_taxon_mapping_path, low_memory=False)
+    content_ids_for_taxon = list(content_taxon_mapping[content_taxon_mapping['taxon_id'] == taxon.content_id]['content_id'])
+    return content[content['content_id'].isin(content_ids_for_taxon)]['combined_text']
+
+def get_score_for_item(combined_text_for_content_item, sentences_for_taxon):
+    sentences_for_taxon_list = sentences_for_taxon.to_list()
+    if not sentences_for_taxon_list:
+        return float("inf")
+    vectorizer = TfidfVectorizer()
+    print(sentences_for_taxon_list)
+    vectorizer.fit(sentences_for_taxon_list)
+    return vectorizer.transform([combined_text_for_content_item]).mean()
+
+import gzip
+import ijson
+import os
+import pandas as pd
+from sklearn.metrics import pairwise_distances_chunked
+import operator
+from sklearn.feature_extraction.text import TfidfVectorizer
+import csv
+
+DATADIR = os.getenv("DATADIR")
+ENCODINGSDATADIR = DATADIR + "/encodings"
+if DATADIR is None:
+    print("You must set a DATADIR environment variable, see the readme in alphagov/govuk-taxonomy-supervised-learning repo for more details")
+    sys.exit()
+
+tree = Tree(DATADIR)
+
+# Load in data
+labelled_file_path = os.path.join(DATADIR, 'labelled.csv.gz')
+labelled = pd.read_csv(labelled_file_path, compression='gzip', low_memory=False)
+
+clean_content_path = os.path.join(DATADIR, 'embedded_clean_content.pkl')
+content = pd.read_pickle(clean_content_path)
+
+problem_content_path = os.path.join(DATADIR, 'problem_content.csv')
+problem_content = pd.read_csv(problem_content_path)
+
+current_scores = {}
+best_fits = {}
+suggested_taxons = {}
+for index, row in problem_content.iterrows():
+    content_to_retag_base_path = row["base_path"]
+    current_taxon = tree.find(row["taxon_id"])
+    if not current_taxon.all_siblings_and_children():
+        # No children or siblings in the same branch for current taxon, at moment just leave it there
+        print("No siblings or children of current taxon, leaving in place: " + content_to_retag_base_path)
+        suggested_taxons[content_to_retag_base_path] = { 'current_taxon': current_taxon.title, 'new_taxon': 'No children!', 'cosine': '?', 'tf_idf': '?' }
+    else:
+        print("attempting_to_retag: " + content_to_retag_base_path)
+        combined_text_for_content_item = content[content['base_path'] == content_to_retag_base_path].iloc[0,:]['combined_text']
+        all_combined_text_for_taxon = get_combined_text_for_taxon(content, current_taxon)
+        # Get the score of the content item against the taxon it's currently in
+        current_score = get_score_for_item(combined_text_for_content_item, all_combined_text_for_taxon)
+        current_scores[content_to_retag_base_path] = current_score
+        # Get the score of the content item against all child items
+        cosine_scores = {}
+        for taxon in current_taxon.all_siblings_and_children():
+            sentences_for_taxon = get_combined_text_for_taxon(content, taxon)
+            cosine_scores[taxon.title] = get_score_for_item(combined_text_for_content_item, sentences_for_taxon)
+        # Sort the scores
+        cosine_scores = sorted(cosine_scores.items(), key=operator.itemgetter(1))
+        cosine_scores.reverse()
+        best_fit = cosine_scores[0]
+        best_fit_name = best_fit[0]
+        best_fit_cosine_score = best_fit[1]
+        # Calculate tf-idf score between content item title and taxon name
+        vectorizer = TfidfVectorizer()
+        vectorizer.fit([best_fit_name])
+        content_item_title = content[content['base_path'] == content_to_retag_base_path].iloc[0,:]['title']
+        tf_idf_score = vectorizer.transform([content_item_title]).mean()
+        # Add to best fits
+        best_fits[content_to_retag_base_path] = {'current_taxon': current_taxon.title, 'new_taxon': best_fit_name, 'cosine': best_fit_cosine_score, 'tf_idf': tf_idf_score}
+        # See if it's suggested
+        # if best_fit_cosine_score < 0.5:
+        #     suggested_taxons[content_to_retag_base_path] = {'current_taxon': current_taxon.title, 'new_taxon': best_fit_name, 'cosine': best_fit_cosine_score, 'tf_idf': tf_idf_score}
+        # if best_fit_cosine_score >= 0.5 and best_fit_cosine_score <= 0.6 and tf_idf_score > 0:
+        #     # For the ~27 items suggested by this process, almost all have been false positives so we'll probably want to remove it
+        #     suggested_taxons[content_to_retag_base_path] = suggested_taxons[content_to_retag_base_path] = {'current_taxon': current_taxon.title, 'new_taxon': best_fit_name, 'cosine': best_fit_cosine_score, 'tf_idf': tf_idf_score}
+
+with open("best_fits.csv", 'w') as csvfile:
+    filewriter = csv.writer(csvfile)
+    filewriter.writerow(['base_path', 'current_taxon', 'new_taxon', 'cosine', 'tf_idf'])
+    for content_base_path, results in best_fits.items():
+        filewriter.writerow([content_base_path, results['current_taxon'], results['new_taxon'], results['cosine'], results['tf_idf']])
+
+with open("suggested_retaggings.csv", 'w') as csvfile:
+    filewriter = csv.writer(csvfile)
+    filewriter.writerow(['base_path', 'current_taxon', 'new_taxon', 'cosine', 'tf_idf'])
+    for content_base_path, results in suggested_taxons.items():
+        filewriter.writerow([content_base_path, results['current_taxon'], results['new_taxon'], results['cosine'], results['tf_idf']])
